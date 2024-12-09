fe6d34a59e5ca780f04a1b25233f25bb9aae4237
ehtishama <ehtishamhassan9@gmail.com>
2024-12-09 13:43:32 +0000

    Added logger.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("MiniDB.js");
+import { logDebug, logError, logInfo, logSuccess } from "./logger/logger.js";
+logError("This is my error");
+logInfo("My info");
+logSuccess("My success");
+logDebug("My debug");
